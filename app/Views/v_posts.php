4e01ec78900098d3ea7092f1c1ad2d859bf1066d
vrvrn <viravrna@gmail.com>
2021-09-23 22:08:46 +0700

    second commit

--- app/Views/v_posts.php
+++ app/Views/v_posts.php
@@ -2,4 +2,79 @@
     <div class="text-center">
         <h1>All Posts</h1>
     </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form action="" method="GET">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Cari postingan" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row mt-4 mb-4">
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?nature" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                    <h5 class="card-title">Nature</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?food" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                    <h5 class="card-title">Food</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?animal" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                    <h5 class="card-title">Animal</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?water" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                    <h5 class="card-title">Water</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?lifestyle" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                    <h5 class="card-title">Lifestyle</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?photo" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                    <h5 class="card-title">Photo</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>

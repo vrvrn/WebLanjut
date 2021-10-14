a20edea4c66bc91361c21760d02395fcbca80d18
vrvrn <62734351+vrvrn@users.noreply.github.com>
2021-10-14 15:27:56 +0700

    fifth commit

---
+++ app/Controllers/AdminPostsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class AdminPostsController extends BaseController
+{
+    public function index()
+    {
+        return view("posts/index");
+    }
+
+    public function create()
+    {
+        return view("posts/create");
+    }
+
+    public function store()
+    {
+        
+    }
+}
